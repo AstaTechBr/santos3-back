187eb05924687869235462891cca64de961629a5
Guilherme-Mascarenhas <guillherme.s.mascarenhas03@gmail.com>
2024-11-20 09:32:31 -0300

    updated server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const sequelize = require('./config/config');
 const imovelRoutes = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', imovelRoutes);
