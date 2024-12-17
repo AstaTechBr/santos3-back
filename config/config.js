7e9ab52061042b00425fc40a7923a9c6e8b84a85
Guilherme-Mascarenhas <guillherme.s.mascarenhas03@gmail.com>
2024-12-17 12:35:41 -0300

    pdated

--- config/config.js
+++ config/config.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
+console.log(process.env.DB_DIALECT);
 
-const sequelize = new Sequelize('santos3', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      logging: false,
+    }
+  );
 
 module.exports = sequelize;
